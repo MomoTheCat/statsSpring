77717e89a8db934bbef46d3d9ec635edbcbd802f
Kacper Piechota <k.piechota@oberthur.com>
2017-03-13 20:21:57 +0100

    Rename tests

--- src/test/java/pl/momothecat/stats/StatsSpringApplicationTestsIT.java
+++ src/test/java/pl/momothecat/stats/StatsSpringApplicationTestsIT.java
@@ -5,18 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StatsSpringApplicationTests {
+public class StatsSpringApplicationTestsIT {
 
     @Value("${minutesto.schedule}")
     private int minutesToSchedule = 10;
 
+
     @Autowired
     @SuppressWarnings("SpringJavaAutowiringInspection")
-    private CollectData collectData;
+    CollectData collectData;
 
     @Test
 	public void contextLoads() {
