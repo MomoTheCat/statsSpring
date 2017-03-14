cd3e239d3f140a179e842671d75cc7cd840a1ac7
Kacper Piechota <k.piechota@oberthur.com>
2017-03-14 15:50:20 +0100

    Refactor Tests

--- src/test/java/pl/momothecat/stats/ApplicationControllerTestIT.java
+++ src/test/java/pl/momothecat/stats/ApplicationControllerTestIT.java
@@ -1,50 +1,50 @@
 package pl.momothecat.stats;
 
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRule;
-import org.mockito.junit.MockitoRule;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.context.WebApplicationContext;
 import pl.momothecat.stats.dao.StationsRepository;
 import pl.momothecat.stats.model.SimpleExtra;
 import pl.momothecat.stats.model.SimpleStation;
 import utils.TestUtils;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 
 /**
  * Created by Kacper on 2017-03-13.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class,EmbeddedMongoAutoConfiguration.class})
 @WebAppConfiguration
 @TestPropertySource(locations="classpath:test.properties")
-public class ApplicationControllerTest {
+public class ApplicationControllerTestIT {
 
 
     private MockMvc mockMvc;
@@ -52,21 +52,17 @@ public class ApplicationControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @Mock
-    private StationsRepository stationsRepositoryMock;
+    @Autowired
+    private StationsRepository stationsRepository;
 
-    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-    SimpleStation simpleStation1;
-    SimpleStation simpleStation2;
 
     @Before
     public void setUp() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
-        //Zamockować zwrotke StationsRepository
-        simpleStation1= TestUtils.prepareSimpleStation("1",15,25,10);
-        simpleStation2=TestUtils.prepareSimpleStation("2",10,35,25);
-
+        MockitoAnnotations.initMocks(this);
+        stationsRepository.deleteAll();
+        stationsRepository.save(TestUtils.prepareSimpleStation("1",15,25,10));
+        stationsRepository.save(TestUtils.prepareSimpleStation("2",10,35,25));
     }
 
     @Test
@@ -85,10 +81,7 @@ public class ApplicationControllerTest {
     }
 
     @Test
-    @Ignore
     public void getAll() throws Exception {
-        when(stationsRepositoryMock.findByIdNetwork("1")).thenReturn(simpleStation1);
-
         mockMvc.perform(get("/get"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().string(containsString("Test Station 1")))
@@ -96,19 +89,15 @@ public class ApplicationControllerTest {
     }
 
     @Test
-    @Ignore
     public void getSingle() throws Exception {
-        when(stationsRepositoryMock.findByIdNetwork("1")).thenReturn(simpleStation1);
-
         mockMvc.perform(get("/get/1"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().string(containsString("Name: Test Station 1")))
-                .andExpect(content().string(containsString("Free bikes: 15")))
-                .andExpect(content().string(containsString("Free slots: 5")));
+                .andExpect(content().string(containsString("Free bikes: 10")))
+                .andExpect(content().string(containsString("Free slots: 15")));
     }
 
     @Test
-    @Ignore
     public void getStationWithBiggestNumberOfBikes() throws Exception {
         mockMvc.perform(get("/getMaxBike"))
                 .andExpect(status().is2xxSuccessful())
--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -1,6 +1,12 @@
 package utils;
 
+import pl.momothecat.stats.model.SimpleExtra;
+import pl.momothecat.stats.model.SimpleStation;
+
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +16,33 @@ import static org.junit.Assert.assertNotNull;
  * Created by szymon on 05.03.2017.
  */
 public class TestUtils {
+    private static Random generator = new Random();
+
+    public static SimpleStation prepareSimpleStation(String networkId,int freeSlots,int allSlots, int bikesCount){
+        int tab[] = new int[bikesCount];
+        for (int i = 0; i<bikesCount; i++) {
+            tab[i]=generator.nextInt();
+        }
+
+        SimpleExtra se = SimpleExtra.newBuilder()
+                .setDate(new Date())
+                .setSlots(allSlots)
+                .setFree_bikes(bikesCount)
+                .setEmpty_slots(freeSlots)
+                .setUid(generator.nextInt())
+                .setBike_uids(tab)
+                .setNumber(generator.nextInt())
+                .build();
+
+        SimpleStation st = SimpleStation.newBuilder()
+                .setIdNetwork(networkId)
+                .setName("Test Station " + networkId)
+                .setExtras(Arrays.asList(se))
+                .setLatitude(50.0)
+                .setLongitude(40.0)
+                .build();
+        return st;
+    }
 
     public static void assertNotEmptyValue(Object object) {
         for (Field field : object.getClass().getDeclaredFields()) {
